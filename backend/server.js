2aa7e48bed086763e19aa2d78f67c0fc6814045b
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-08 18:00:12 -0300

    init

---
+++ backend/server.js
@@ -0,0 +1,74 @@
+
+import path from "path";
+const __dirname = import.meta.dirname;
+
+
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {
+  date: 'test',
+  place: 'test',
+  temperature: 'test',
+  feelings: 'test'
+};
+
+let results = [];
+
+// Require Express to run server and routes
+import express from "express";
+import cors from "cors";
+
+// Start up an instance of app
+
+const app = express();
+const port = 3010;
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(express.urlencoded());
+app.use(express.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static(path.resolve(__dirname,'../dist')));
+
+app.get('/test',(req,res)=>{
+  res.send('Success!');
+})
+
+function setProjectData(receivedData) {
+  
+  projectData = receivedData;
+  results = [projectData, ...results];
+  // projectData.date = receivedData.date;
+  // projectData.place = receivedData.place;
+  // projectData.temperature = receivedData.temperature;
+  // projectData.user_feeling = receivedData.user_feeling;
+}
+
+
+app.get('/all', (req,res)=>{
+console.log(`inside /all ${results[0].place}`)
+  res.send(results);
+})
+
+app.post('/setData', (req,res)=>{
+  try {
+    const receivedData = req.body;
+    if (!receivedData) {throw new Error('Nenhum conteúdo foi recebido')}
+    if (Object.keys(receivedData).length !== 4) {throw new Error('Dado incorreto enviado')}
+    
+    //Em caso de passar pelos testes de erro:
+    setProjectData(receivedData);
+    res.send({success:true, message: 'dados recebidos corretamente'}); 
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({success:false, message: error.message});
+  }
+})
+
+// Setup Server
+app.listen(port, ()=>{
+  console.log(`server up on port ${port}`);
+})
---
+++ frontend/app.js
@@ -0,0 +1,80 @@
+/* zip for tests:  
+  miami: 33101 
+33125-1234 (Miami)
+  Orlando: 32801
+32801-5678 (Orlando)
+
+10001-0001 (New York)
+10001 (Manhattan)
+
+https://api.openweathermap.org/data/2.5/weather?zip=32801,us&appid=dd6c3ba86f66f547459582b843e14bc8
+*/
+
+//imports
+import axios from "axios";
+import getCurrentDateTime from "./date_time.js";
+
+/* Global Variables */
+const baseURL = "https://api.openweathermap.org/data/2.5/weather";
+const apikey = 'dd6c3ba86f66f547459582b843e14bc8';
+
+async function setUI() {
+  const {data:allResults} = await axios.get('http://localhost:3010/all');
+  const allEntriesDiv = document.querySelector("#allEntries")
+  allEntriesDiv.innerHTML = '';
+  for (const result of allResults) {
+    const entryDiv = document.createElement('div');
+    entryDiv.classList.add('entryHolder');
+    entryDiv.innerHTML =
+    ` <div class ="place">${result.place}</div>
+      <div class = "date">${result.date}</div> 
+      <div class = "temperature">Temp: ${result.temperature} degres</div>
+      <div class = "feelings">${result.feelings}</div>
+      <p>-----------</p>
+    `
+    allEntriesDiv.appendChild(entryDiv);
+  }
+}
+
+function toCelsius(k) {
+  return k - 273.15;
+}
+
+async function getUserData(evt) {
+  evt.preventDefault();
+  const zip = document.getElementById('zip').value;
+  const feelings = document.getElementById('feelings').value;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=dd6c3ba86f66f547459582b843e14bc8`
+  const {data:result} = await axios.get(weatherUrl);
+  const temperature = toCelsius(result.main.temp);
+  const resultObj = {
+    feelings: feelings,
+    place: result.name,
+    temperature: Math.round(temperature),
+    date: getCurrentDateTime()
+  }
+  await axios.post('http://localhost:3010/setData', resultObj);
+  await setUI();
+}
+
+const form = document.getElementById('entriesForm');
+form.addEventListener('submit',getUserData);
+
+
+function setCurrentYearInFooter() {
+  const spanElement = document.querySelector("#this-year");
+  const thisYear = new Date().getFullYear();
+  //console.log('test');
+  //console.log(thisYear);
+  spanElement.textContent = thisYear;
+}
+
+async function getWeather(zipCode) {
+  const fullUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=dd6c3ba86f66f547459582b843e14bc8`;
+  const {data:result} = await axios.get(fullUrl);
+  //console.log(result.name);
+}
+
+setCurrentYearInFooter();
+//getWeather(33101);
+
