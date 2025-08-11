2f6f14f56bf8f029049e9ee8d067fd1445027009
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-11 05:40:51 -0300

    adjusts

---
+++ dist/main.a69f71f3e4f89f59e02b.hot-update.js
@@ -0,0 +1,37 @@
+self["webpackHotUpdatems1_weather_journal_app"]("main",{
+
+/***/ "./frontend/styles/style.css":
+/*!***********************************!*\
+  !*** ./frontend/styles/style.css ***!
+  \***********************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Unexpected token (1:5)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n> html {\n|   box-sizing: border-box;\n| }");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _frontend_styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../frontend/styles/style.css */ "./frontend/styles/style.css");
+/* harmony import */ var _frontend_js_app_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../frontend/js/app.js */ "./frontend/js/app.js");
+
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("43fdc1ad609c8ff9d270")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.a69f71f3e4f89f59e02b.hot-update.js.map
