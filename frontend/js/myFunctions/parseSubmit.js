47823f203a59d3499cd922282db0f9d7bdf960d8
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-11 16:20:49 -0300

    get image from pixabay

--- frontend/js/myFunctions/parseSubmit.js
+++ frontend/js/myFunctions/parseSubmit.js
@@ -29,11 +29,34 @@ function setUI() {
   document.querySelector('#result .description').textContent = `${projData.weather.description}, ${projData.country}`
 }
 
+function getRandomNr(totalHits) {
+  if (totalHits>10){
+    return Math.floor((10)*Math.random());
+  } else {
+    return 0;
+  }
+    
+}
+
+async function getImage(place) {
+  const url = `https://pixabay.com/api/?key=36921349-37b715f1fda946c5428d9d405&q=${place}&image_type=photo`;
+  const result = await axios.get(url);
+  const randomImg = getRandomNr(result.data.totalHits);
+  console.log(`O nr é : ${randomImg}`);
+  const imgUrl = result.data.hits[randomImg].largeImageURL;
+  console.log(imgUrl);
+  const altForImg = `a random image from pixabay site for ${place}`;
+  const image = document.querySelector("#show-image img");
+  image.setAttribute('alt',altForImg);
+  image.setAttribute('src',imgUrl);
+}
+
 
 export default async function onSubmitForm(evt) {
   evt.preventDefault();
   const placeToGo = document.querySelector('#place-to-go').value;
   const {lat, lng} = await callGeonamesApi(placeToGo);
   await callWeatherbitApi(lat,lng);
+  await getImage(placeToGo);
   setUI()
 }
