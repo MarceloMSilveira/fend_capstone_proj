2fe8eb9683a495c6424951e85620e04af81aff82
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-11 14:15:32 -0300

    calling weatherbitApi

--- frontend/js/myFunctions/parseSubmit.js
+++ frontend/js/myFunctions/parseSubmit.js
@@ -5,17 +5,28 @@ async function callGeonamesApi(placeToGo) {
   const place = encodeURIComponent(placeToGo);
   const url = `http://api.geonames.org/searchJSON?q=${place}&maxRows=10&username=MarceloMSilveira`;
   const allData = await axios.get(url);
-  const {countryCode, name:cityName, lng, lat} = allData.data.geonames[0]
+  const {countryCode, name:cityName, lng, lat} = allData.data.geonames[0];
+  projData.city = cityName;
+  projData.country = countryCode;
+  return {lat, lng}
+}
 
-  //console.log(allData.data.geonames[0].name);
-  const spanElement = document.querySelector("#result span");
-  spanElement.textContent = `${cityName}, ${countryCode}`;
+async function callWeatherbitApi(lat,lng) {
+  console.log(`lat: ${lat}`);
+  console.log(`lng: ${lng}`);
+  const apiKey = '7bcbccd81e84418d8552d07ba5baf291';
+  const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lng}&key=${apiKey}`;
+
+  const allData = await axios.get(url);
+  const {weather, temp} = allData.data.data[0];
+  projData.weather.description = weather.description;
+  projData.weather.high = temp;
 }
 
 
-export default function onSubmitForm(evt) {
+export default async function onSubmitForm(evt) {
   evt.preventDefault();
   const placeToGo = document.querySelector('#place-to-go').value;
-  callGeonamesApi(placeToGo);
-  projData.city = placeToGo;
+  const {lat, lng} = await callGeonamesApi(placeToGo);
+  callWeatherbitApi(lat,lng);
 }
