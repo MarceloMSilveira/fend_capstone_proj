1675c0f48751913094a0f1aec7f53077df34634e
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-13 17:42:27 -0300

    set save a trip

--- frontend/js/myFunctions/setNewBtn.js
+++ frontend/js/myFunctions/setNewBtn.js
@@ -1,7 +1,12 @@
 export default function addSettingsToNewBtn() {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('user-trip');
+
   $("#save-trip").on('click', function () {
   const htmlString = $("#weather").html();
   console.log(htmlString);
-  $(".saved-trips").html(htmlString);
+  newDiv.innerHTML = htmlString;
+  const div = document.querySelector('.saved-trips');
+  div.appendChild(newDiv);
 });
 }
