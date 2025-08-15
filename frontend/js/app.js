4127246b97cdcdc45b691036d4014beae50f019b
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-15 09:14:19 -0300

    add localstorage and separate show trips

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,6 @@
 import setCurrentYearInFooter from "./myFunctions/yearToFooter.js";
 import onSubmitForm from "./myFunctions/parseSubmit.js";
+import showAllTrips from "./myFunctions/showUserTrips.js";
 
 export const trip = {
   city: '',
@@ -17,6 +18,12 @@ export const trip = {
 };
 
 export const trips = [];
+
+const previousTrips = localStorage.getItem('tripsOnLocalStorage');
+
+if (previousTrips) {
+  showAllTrips();
+}
 
 const submitElement = document.querySelector("#submit-btn");
 submitElement.addEventListener('click',onSubmitForm);
