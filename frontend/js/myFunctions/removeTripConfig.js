54f9a5b5b30d0f1ee8c36ddee8c4fcc724cc8702
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-15 14:52:53 -0300

    remove trip done

--- frontend/js/myFunctions/removeTripConfig.js
+++ frontend/js/myFunctions/removeTripConfig.js
@@ -1,8 +1,24 @@
+import { trips } from "../app.js";
+import showAllTrips from "./showUserTrips.js";
+
 export default function removeTripConfig() {
   const deleteBtns = document.querySelectorAll('.delete-trip');
   deleteBtns.forEach( (deleteBtn)=>{
     deleteBtn.addEventListener('click',(evt)=>{
       const id = evt.target.parentElement.getAttribute('id');
+      const cityToRemove = trips.filter(trip => trip.id === id)[0].city;
+      const newTrips = trips.filter(trip => trip.id !== id);
+      trips.length=0;
+      trips.push(...newTrips);
+      alert(`${cityToRemove} was removed!`)
+      //console.log(trips);
+      //agora eu tenho que substituir o array que está no local storage.
+
+      //saving trips no localstorage:
+      localStorage.setItem('tripsOnLocalStorage', JSON.stringify(trips));
+
+      //mandar imprimir as viagens do usuário
+      showAllTrips()
     });
   })
 }
