c019377a0c807279854bae70ada9c89b24cd1c74
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-17 17:12:08 -0300

    show trip details done

--- frontend/js/myFunctions/showOneSavedTrip.js
+++ frontend/js/myFunctions/showOneSavedTrip.js
@@ -1,18 +1,19 @@
 import { trips } from "../app.js";
+import setPreviewUI from "./previewUISettings.js";
 
 export default function showOneTripConfig() {
   const showOneTripBtns = document.querySelectorAll('.show-trip');
   showOneTripBtns.forEach((showBtn)=>{
     showBtn.addEventListener('click',(evt)=>{
       const id = evt.target.parentElement.getAttribute('id');
-      const cityToShow = trips.filter(trip => trip.id === id)[0].city;
-      const urlToShow = trips.filter(trip => trip.id === id)[0].imgUrl;
-      console.log(cityToShow);
-      console.log(urlToShow);
+      const trip = trips.filter(trip => trip.id === id)[0];
+      const cityToShow = trip.city;
+      const urlToShow = trip.imgUrl;
       const altForImg = `a random image from pixabay site for ${cityToShow}`;
       const image = document.querySelector("#show-image img");
       image.setAttribute('alt',altForImg);
       image.setAttribute('src',urlToShow);
+      setPreviewUI(trip)
     })
   })
 }
