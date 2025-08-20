04ff9a3d421992763971a666cf47c3b7a907d1b9
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-20 19:27:51 -0300

    set notes

--- frontend/js/myFunctions/saveTrip.js
+++ frontend/js/myFunctions/saveTrip.js
@@ -14,5 +14,4 @@ export default function saveNewTrip() {
   saveImageToCache(trip.imgUrl);
   //mandar imprimir as viagens do usuário
   showAllTrips()
-
 }
--- frontend/js/myFunctions/utils/setNotes.js
+++
@@ -1,3 +0,0 @@
-export default function setNotes() {
-  alert('Config a note!');
-}
